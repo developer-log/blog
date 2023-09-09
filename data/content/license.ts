40715c682fa77282050fb7a71e64bdbd9f03b72e
Даниил Шило <tokiory.personal@gmail.com>
2023-09-09 22:32:53 +0300

    feat(license): add license page

--- data/content/license.ts
+++ data/content/license.ts
@@ -3,79 +3,84 @@ import type { LicenseContent } from "@t/content";
 const packages: LicenseContent = [
   {
     title: "Nuxt",
-    source: "",
-    license: "",
+    source: "https://github.com/nuxt/nuxt",
+    license: "https://github.com/nuxt/nuxt/blob/main/LICENSE",
   },
   {
     title: "Vue 3",
-    source: "",
-    license: "",
+    source: "https://github.com/vuejs/core",
+    license: "https://github.com/vuejs/core/blob/main/LICENSE",
   },
   {
     title: "Typescript",
-    source: "",
-    license: "",
+    source: "https://github.com/microsoft/TypeScript/#readme",
+    license: "https://github.com/microsoft/TypeScript/blob/main/LICENSE.txt",
   },
   {
-    title: "Ava",
-    source: "",
-    license: "",
+    title: "Vitest",
+    source: "https://github.com/vitest-dev/vitest",
+    license: "https://github.com/vitest-dev/vitest/blob/main/LICENSE",
   },
   {
     title: "Playwright",
-    source: "",
-    license: "",
-  },
-  {
-    title: "Vercel",
-    source: "",
-    license: "",
+    source: "https://github.com/microsoft/playwright",
+    license: "https://github.com/microsoft/playwright/blob/main/LICENSE",
   },
   {
     title: "ESLint",
-    source: "",
-    license: "",
+    source: "https://github.com/eslint/eslint",
+    license: "https://github.com/eslint/eslint/blob/main/LICENSE",
   },
   {
     title: "Stylelint",
-    source: "",
-    license: "",
+    source: "https://github.com/stylelint/stylelint",
+    license: "https://github.com/stylelint/stylelint/blob/main/LICENSE",
+  },
+  {
+    title: "Vercel",
+    source: "https://vercel.com/",
+    license: "https://vercel.com/legal/terms",
   },
 ];
 
 const fonts: LicenseContent = [
   {
     title: "Iosevka",
-    source: "",
-    license: "",
+    source: "https://github.com/be5invis/Iosevka",
+    license: "https://github.com/be5invis/Iosevka/blob/main/LICENSE.md",
   },
   {
     title: "Montserrat",
-    source: "",
-    license: "",
+    source: "https://github.com/JulietaUla/Montserrat",
+    license: "https://github.com/JulietaUla/Montserrat/blob/master/OFL.txt",
   },
 ];
 
 const icons: LicenseContent = [
   {
+    title: "Devicon",
+    source: "https://github.com/devicons/devicon",
+    license: "https://github.com/devicons/devicon/blob/master/LICENSE",
+  },
+  {
     title: "VSCode Icons",
-    source: "",
-    license: "",
+    source: "https://github.com/vscode-icons/vscode-icons",
+    license: "https://creativecommons.org/licenses/by-sa/4.0/",
   },
   {
     title: "Iconify",
-    source: "",
-    license: "",
+    source: "https://github.com/iconify/iconify",
+    license: "https://github.com/iconify/iconify/blob/main/license.txt",
   },
   {
     title: "Feather Icons",
-    source: "",
-    license: "",
+    source: "https://github.com/feathericons/feather",
+    license: "https://github.com/feathericons/feather/blob/main/LICENSE",
   },
   {
     title: "Unicons",
-    source: "",
-    license: "",
+    source: "https://github.com/Iconscout/unicons",
+    license: "https://iconscout.com/licenses#simple_license",
   },
 ];
 
